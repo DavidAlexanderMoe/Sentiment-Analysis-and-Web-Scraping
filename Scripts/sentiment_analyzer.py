4374e0ec94f81f51f3e8072cbe9bd38f6d195d69
DavidAlexanderMoe <Davidalexander100@hotmail.com>
2023-06-07 01:46:36 +0200

    discard

--- Scripts/sentiment_analyzer.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-st.title('Sentiment Analyzer App')
-st.write('Welcome to my sentiment analysis app!')
-form = st.form(key='sentiment-form')
-user_input = form.text_area('Enter your text')
-submit = form.form_submit_button('Submit')
